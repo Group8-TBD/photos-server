5a466691566e7a48a763c7fd20961852310364cc
Patrick Mac <patmac510@Patricks-Air.attlocal.net>
2020-04-03 22:37:30 -0700

    created a new seed file that generates data for my new databases

---
+++ db/seed_data.js
@@ -0,0 +1,23 @@
+const faker = require('faker')
+
+const bucketUrl = 'https://mtnolympusimages.s3-us-west-1.amazonaws.com/'
+
+let createImage =  (url, i, j) => {
+  let description = faker.lorem.sentence();
+  url = url + i + "/" + j;
+  const newImage = {property_id: j, url: url + ".jpg", description: description};
+  return newImage;
+}
+
+let createBundle = (url) => {
+  for (let i = 0; i < 100; i++) {
+    let imageArray = [];
+    for (let j = 0; j < 21; j++) {
+      const newImage = createImage(url, i, j);
+      imageArray.push(newImage)
+    }
+  }
+  return imageArray
+}
+
+const photoBucket = createBundle(bucketUrl);
