de7c1aa6269793b928c55c519648562d8c7b3b17
Patrick Mac <patmac510@Patricks-Air.attlocal.net>
2020-04-04 14:19:32 -0700

    made a createdRecords function for csv

--- db/cassandra_seeder/seed.js
+++ db/cassandra_seeder/seed.js
@@ -1,13 +1,38 @@
 const photoBucket = require('../seed_data.js');
 const createCvsWriter = require('csv-writer').createObjectCsvWriter;
+const faker = require('faker')
+
 
 
 const csvWriter = createCsvWriter({
   path: './db/cassandra_seeder/seed_records/files.csv',
   header: [
     {id: 'property_id', title: 'property_id'},
+    {id: 'image_id', title: 'image_id'},
     {id: 'url', title: 'url'},
     {id: 'description', title: 'description'}
   ]
 })
+
+let propID = 1;
+let imageID = 1;
+
+const createRecords = () => {
+  let records = [];
+  for (var i = 0; i < photoBucket.length; i++) {
+    for (var j = 0; j < photoBucket[i].length; j++) {
+      let imageUrl = photoBucket[i][j];
+      let record = {
+        property_id: propID,
+        image_id: imageID,
+        url: imageUrl,
+        description: faker.lorem.sentence(),
+      }
+      records.push(record)
+      imageID++
+    }
+    propID++
+  }
+  return records;
+}
 
--- db/seed_data.js
+++ db/seed_data.js
@@ -1,25 +1,25 @@
-const faker = require('faker')
 
 const bucketUrl = 'https://mtnolympusimages.s3-us-west-1.amazonaws.com/'
 
 let createImage =  (url, i, j) => {
-  let description = faker.lorem.sentence();
   url = url + i + "/" + j;
-  const newImage = {property_id: j, url: url + ".jpg", description: description};
+  const newImage = url + ".jpg";
   return newImage;
 }
 
 let createBundle = (url) => {
-  for (let i = 0; i < 100; i++) {
+  const container = [];
+  for (let i = 0; i < 10; i++) {
     let imageArray = [];
     for (let j = 0; j < 21; j++) {
       const newImage = createImage(url, i, j);
       imageArray.push(newImage)
     }
+    container.push(imageArray);
   }
-  return imageArray
+  return container;
 }
 
 const photoBucket = createBundle(bucketUrl);
-
+console.log(photoBucket)
 module.exports = photoBucket;
